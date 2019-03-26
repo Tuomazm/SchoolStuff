1e0f586c176cbf29a59ad3f1d8a841d443bcfa67
Tuomas Makela <tuomas.makela@metropolia.fi>
2019-03-26 14:15:58 +0200

    final

--- Composite/src/composite/AbstractFactory.java
+++ Composite/src/composite/AbstractFactory.java
@@ -1,36 +1,10 @@
 package composite;
 
-import java.util.Random;
-
 public abstract class AbstractFactory {
 
-	private Random rand;
-
-	public AbstractFactory() {
-		rand = new Random();
-	}
-
-	public Tietokone kasaaTietokone() {
-		Tietokone kone = new Tietokone();
-		kone.addLaiteosa(luoKotelo());
-		kone.addLaiteosa(luoEmolevy());
-		return kone;
-	}
-
-	private Laiteosa luoKotelo() {
-		Laiteosa kotelo = new Kotelo(rand.nextInt(100) + lisaaKate());
-		return kotelo;
-	}
-
-	private Laiteosa luoEmolevy() {
-		Laiteosa emolevy = new Emolevy(rand.nextInt(100) + lisaaKate());
-		emolevy.addLaiteosa(new Muistipiiri(rand.nextInt(100) + lisaaKate()));
-		emolevy.addLaiteosa(new Prosessori(rand.nextInt(100) + lisaaKate()));
-		emolevy.addLaiteosa(new Verkkokortti(rand.nextInt(50) + lisaaKate()));
-		emolevy.addLaiteosa(new Näytönohjain(rand.nextInt(100) + lisaaKate()));
-		return emolevy;
-	}
-	
+	public abstract Tietokone kasaaTietokone();
+	public abstract Laiteosa luoKotelo();
+	public abstract Laiteosa luoEmolevy();
 	public abstract double lisaaKate();
 
 }
--- Composite/src/composite/FujitsuFactory.java
+++ Composite/src/composite/FujitsuFactory.java
@@ -1,7 +1,39 @@
 package composite;
 
-public class FujitsuFactory extends AbstractFactory{
+import java.util.Random;
 
+public class FujitsuFactory extends AbstractFactory{
+	
+	private Random rand;
+
+	public FujitsuFactory(){
+		rand = new Random();
+	}
+	
+	@Override
+	public Laiteosa luoEmolevy() {
+		Laiteosa emolevy = new Emolevy(rand.nextInt(100) + lisaaKate());
+		emolevy.addLaiteosa(new Muistipiiri(rand.nextInt(100) + lisaaKate()));
+		emolevy.addLaiteosa(new Prosessori(rand.nextInt(100) + lisaaKate()));
+		emolevy.addLaiteosa(new Verkkokortti(rand.nextInt(50) + lisaaKate()));
+		emolevy.addLaiteosa(new Näytönohjain(rand.nextInt(100) + lisaaKate()));
+		return emolevy;
+	}
+	
+	@Override
+	public Laiteosa luoKotelo() {
+		Laiteosa kotelo = new Kotelo(rand.nextInt(100) + lisaaKate());
+		return kotelo;
+	}
+	
+	@Override
+	public Tietokone kasaaTietokone() {
+		Tietokone kone = new Tietokone();
+		kone.addLaiteosa(luoKotelo());
+		kone.addLaiteosa(luoEmolevy());
+		return kone;
+	}
+	
 	@Override
 	public double lisaaKate() {
 		return 150;
