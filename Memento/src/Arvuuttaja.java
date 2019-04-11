5410eb0ad31ef72e642670710c52e04f847d1234
Tuomas <36504515+Tuomazm@users.noreply.github.com>
2019-04-11 11:45:56 +0300

    Add files via upload

---
+++ Memento/src/Arvuuttaja.java
@@ -0,0 +1,38 @@
+import java.util.Random;
+
+public class Arvuuttaja {
+
+	private Random random;
+
+	public Arvuuttaja() {
+		random = new Random();
+	}
+
+	public void liityPeliin(Asiakas asiakas) {
+		asiakas.setMemento(new Memento(random.nextInt(10) + 1));
+	}
+
+	public boolean arvaaLuku(Asiakas asiakas, int arvaus) {
+		Memento memento = (Memento) asiakas.getMemento();
+		if (arvaus == memento.getLuku()) {
+			System.out.println("Pelaaja arvasi oikein! Oikea vastaus: " + arvaus);
+			return true;
+		} else {
+			System.out.println("Pelaaja arvasi väärin arvatessaan: " + arvaus);
+		}
+		return false;
+	}
+
+	private class Memento {
+
+		private int luku;
+
+		public Memento(int luku) {
+			this.luku = luku;
+		}
+
+		public int getLuku() {
+			return luku;
+		}
+	}
+}
